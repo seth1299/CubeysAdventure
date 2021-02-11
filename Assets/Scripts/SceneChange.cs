278dd3dbfbba9162a207f4a660cb5184cee83556
jwise1596 <53056751+jwise1596@users.noreply.github.com>
2021-02-11 18:05:04 -0500

    newest

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using UnityEngine.SceneManagement;  
+public class SceneChange: MonoBehaviour {  
+    public void Scene1() {  
+        SceneManager.LoadScene("MainMenu");  
+    }  
+    public void Scene2() {  
+        SceneManager.LoadScene("HowToPlay");  
+    }  
+    public void Scene3() {  
+        SceneManager.LoadScene("Credits");  
+    }  
+    public void Scene4() {  
+        SceneManager.LoadScene("LevelOne");  
+    }  
+} 
